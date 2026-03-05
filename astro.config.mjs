b9d32b79be06e476e4dcfbf70b37985168065b4c
gerardPolloRebozado <gerardit4@gmail.com>
2026-03-05 18:39:17 +0100

    fix: :bug: blogs not reading language correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://gerarddupre.dev',
   integrations: [mdx(), sitemap()],
   i18n: {
     defaultLocale: 'en',
