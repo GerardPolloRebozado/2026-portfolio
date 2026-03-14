63a8c14970ecefad79f97ac316758df24ad3bfd9
gerardPolloRebozado <gerardit4@gmail.com>
2026-03-14 10:43:15 +0100

    fix: :bug: sitemap was not localized

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,18 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gerarddupre.dev',
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          es: 'es',
+        },
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
